da0695b322fc39ae42d0022ca3dfac2c5caab2bc
Aayushi <aayushiiiii09@gmail.com>
2022-10-22 01:52:06 +0530

    merged

--- frontend/src/routes/Router.js
+++ frontend/src/routes/Router.js
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Navbar from '../components/Navbar/Navbar'
 import About from '../pages/About/About'
 import Home from '../pages/Home/Home'
-// import UserMenu from '../pages/UserMenu/UserMenu'
+import UserMenu from '../pages/UserMenu/UserMenu'
 import User from '../pages/User/User'
 import { ROUTES } from './RouterConfig'
 
@@ -42,11 +42,11 @@ const Router = () => {
           path={ROUTES.User}
           element={<RouteWithoutNavbar Element={User} />}
         ></Route>
-        {/* <Route
+        <Route
           exact
           path={ROUTES.UserMenu}
           element={<RouteWithoutNavbar Element={UserMenu} />}
-        ></Route> */}
+        ></Route>
       </Routes>
     </div>
   )
